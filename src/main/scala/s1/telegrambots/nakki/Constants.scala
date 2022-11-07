2ed2a7eac4db8a7cdf6b33e32928d702f536cb59
valentinep <topias.paju@aalto.fi>
2022-11-07 15:58:35 +0200

    testing

--- src/main/scala/s1/telegrambots/nakki/Constants.scala
+++ src/main/scala/s1/telegrambots/nakki/Constants.scala
@@ -1,5 +1,8 @@
 package s1.telegrambots.nakki
 
 object Constants {
+
+  // The length of the event invite code
   val AccessCodeLength = 6
+
 }
--- src/main/scala/s1/telegrambots/nakki/Event.scala
+++ src/main/scala/s1/telegrambots/nakki/Event.scala
@@ -3,6 +3,7 @@ package s1.telegrambots.nakki
 import util.Random.alphanumeric
 
 class Event(var name: String, var description: String, id: Int ) {
-
+  
   val accessCode = alphanumeric.take(Constants.AccessCodeLength).mkString
+
 }
